00156f31072630e013c1be44f95681eb4c70e9cf
Aashish Kumar Ray <2002raysi@gmail.com>
2024-09-18 20:57:19 +0530

    JDBC Pratices

---
+++ Java/JDBC/src/LoginSignupHandeling.java
@@ -0,0 +1,10 @@
+public class LoginSignupHandeling {
+    private static final String url = "jdbc:mysql://localhost:3306/User";
+    private static final String username = "root";
+    private static final String password = "Raysi@2002";
+
+    public static void main(String[] args) {
+
+    }
+
+}
